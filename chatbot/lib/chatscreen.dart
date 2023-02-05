08a58c072c48ee56b5d9ac31668b06fe1a0f984f
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-05 19:32:34 +0530

    Implemented the working using ChatGPT API

--- chatbot/lib/chatscreen.dart
+++ chatbot/lib/chatscreen.dart
@@ -1,5 +1,8 @@
+import "dart:async";
 import "dart:ui";
+import "package:chat_gpt_sdk/chat_gpt_sdk.dart";
 import "package:chatbot/chatmessage.dart";
+import "package:chatbot/threedots.dart";
 import "package:flutter/material.dart";
 import "package:velocity_x/velocity_x.dart";
 
@@ -13,13 +16,46 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   final List<ChatMessage> _messages = [];
+  ChatGPT? chatGPT;
+
+  StreamSubscription? _subscription;
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 
   void _sendMessage() {
-    ChatMessage _message = ChatMessage(text: _controller.text, sender: "user");
+    ChatMessage message = ChatMessage(text: _controller.text, sender: "user");
     setState(() {
-      _messages.insert(0, _message);
+      _messages.insert(0, message);
+      _isTyping = true;
     });
+
     _controller.clear();
+    final request = CompleteReq(
+        prompt: message.text, model: kTranslateModelV2, max_tokens: 200);
+    // API Key-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
+    _subscription = chatGPT!
+        .builder("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")
+        .onCompleteStream(request: request)
+        .listen((response) {
+      Vx.log(response!.choices[0].text);
+      ChatMessage botMessage =
+          ChatMessage(text: response.choices[0].text, sender: "bot");
+
+      setState(() {
+        _isTyping = false;
+        _messages.insert(0, botMessage);
+      });
+    });
   }
 
   Widget _buildTextComposer() {
@@ -57,6 +93,10 @@ class _ChatScreenState extends State<ChatScreen> {
                     return _messages[index];
                   },
                 ),
+              ),
+              if(_isTyping) ThreeDots(),
+              const Divider(
+                height: 1.0,
               ),
               Container(
                   decoration: BoxDecoration(color: context.cardColor),
